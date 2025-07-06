63b255d309509158d013f7f6cfeeb029db437b2f
Arham Afzal <wwwarhamafzal682@gmail.com>
2025-07-06 11:31:27 +0500

    first_commit

---
+++ input.py
@@ -0,0 +1,4 @@
+num=input("Enter a number : ")
+number=int(num)
+number=number*2
+print(number)
--- variables.py
+++ variables.py
@@ -1,1 +1,8 @@
+#Hello world program
 print("Hello World")
+#Adding two numbers and printin their sum with type
+a = 1
+b = 2
+sum=a+b
+print(sum)
+print(type(sum))
