9b09f359bf26fee37747f90bd7189ab6f97d90ff
Arham Afzal <wwwarhamafzal682@gmail.com>
2025-07-06 11:57:40 +0500

    first_commit

---
+++ differen_printing_methods.py
@@ -0,0 +1,16 @@
+a="1"
+name="arham"
+print(name)
+#printing in upper case
+print(name.upper())
+#printing in capitalize
+print(name.capitalize())
+#printing how many times a letter repeated in a string
+print(name.count("a"))
+#Return True if the string is an alpha-numeric string, False otherwise.
+print(name.isalnum())
+#Return True if the string is a digit string, False otherwise
+print(a.isdigit ())
+#Return True if S ends with the specified suffix, False otherwise.
+print(name.endswith("a"))
+print(name.endswith("m"))
